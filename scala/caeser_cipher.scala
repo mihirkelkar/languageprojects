f15f9aff43a54158a8997211326984c19b485467
mihirkelkar <kelkarmhr@gmail.com>
2014-05-24 22:06:11 -0700

    scala code update for caeser cipher

--- scala/caeser_cipher.scala
+++ scala/caeser_cipher.scala
@@ -1,8 +1,16 @@
 println("Enter text to be encrypted")
-var text = readLine()
+var text = readLine().toLowerCase
 println("Enter shifting number")
 var shift = readInt()
+//println(text)
 //println(shift)
-for(i <- 0 to text.length - 1)
-	println(i)
-
+var cipher = ""
+for(x <- 0 to text.length - 1){
+	var temp = text(x).toInt + shift % 26
+	if(temp > 'z'.toInt){
+		temp = temp % 'z'.toInt
+		temp = 'a'.toInt + temp - 1
+	}
+	cipher += temp.toChar
+}
+println(cipher)
