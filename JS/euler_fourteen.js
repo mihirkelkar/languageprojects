b58b520ea0a63f186a6a178e5d88ae03615ff1d7
Mihir Kelkar <mihirkelkar@Mihirs-MacBook-Pro.local>
2014-08-25 21:47:24 -0700

    Adding problem number fourteen from project euler coded in JS

--- JS/euler_fourteen.js
+++ JS/euler_fourteen.js
@@ -1,1 +1,26 @@
+//Writing code for the collatz conjecture in JAvaScript. This is Project Euler Problem no 14
 
+//n -> n / 2
+//n -> 3n + 1
+
+var i = 0;
+var max_count = 0;
+var max_count_holder = 1;
+for(i = 1; i < 1000000; i++){
+  var number = i;
+  var counter = 0;
+  while(number != 1){
+    counter++;
+    if(number % 2 == 0){
+      number = number / 2;
+    }
+    else{
+      number = 3 * number + 1;
+    }
+  }
+  if(counter > max_count){
+    max_count = counter;
+    max_count_holder  = i;
+  }
+}
+debug("The longest collatz series until a million is by" + max_count_holder);
