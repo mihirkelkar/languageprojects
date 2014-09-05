6ee8fed536e63644a5dfd3f8e37eba5e736c98b2
mihirkelkar <kelkarmhr@gmail.com>
2014-09-05 16:06:01 -0400

    sum of prime numbers code in scala

--- scala/sum_of_primes.scala
+++ scala/sum_of_primes.scala
@@ -1,20 +1,31 @@
 import util.control.Breaks._
+println("This is scala code and you are watching JackAss")
 var counter = 0
 var prime = 3
 var sum = 2
 while(true){
+  //println("=====================")
+  //println("counter is" + counter)
+  //println("=====================")
   var prime_sqrt_round = Math.sqrt(prime).toInt
   var flag = 0
   for(ii <- 2 to prime_sqrt_round){
     if(prime % ii == 0){
       flag = 1
     }
+  }
   if(flag == 0){
+    println("==========")
+    println(prime)
     sum += prime
     counter += 1
    }
-  }
   if(counter == 1000){
+    //println("****************")
+    //println("****************")
+    //println("counter is 1000")
+    //println("****************")
+    //println("****************")
     println(sum)
     break
   }
