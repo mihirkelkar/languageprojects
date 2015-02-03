e1bb2da0ef12d478d206acf17aab8e9e58c5fce2
mihirkelkar <kelkarmhr@gmail.com>
2015-02-03 09:52:18 -0500

    EPI Problem 12.1

---
+++ StInt/EPI/Search/Problem_12_1/README.md
@@ -0,0 +1,1 @@
+Write a method that takes a sorted array A and a key k and returns the index of the first occurence of k in A. Return -1 if the key does not exist. 
---
+++ StInt/EPI/Search/Problem_12_1/first_occurence.py
@@ -0,0 +1,23 @@
+def return_first_occurence(array, key):
+  left = 0
+  right = len(array) -1 
+  while(left < right):
+    middle = (left + right) / 2
+    if array[middle] < key:
+      left = middle
+    elif array[middle] > key:
+      right = middle
+    elif array[middle] == key:
+      while(array[middle - 1] == array[middle]):
+        middle = middle - 1
+      return middle
+
+
+def main():
+  #test case for the first ocurence. 
+  array = [14, -10, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+  print return_first_occurence(array, -10)
+
+if __name__ == "__main__":
+  main()
+
